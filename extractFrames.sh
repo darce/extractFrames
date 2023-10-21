b91d888efe9d410388359236f81e785151dcb06f
Daniel Arcé <daniel.arce@gmail.com>
2023-10-21 12:13:16 -0400

    remove hardware accel & fix init message

--- extractFrames.sh
+++ extractFrames.sh
@@ -20,8 +20,6 @@ input_file=$(basename "$file_path")
 
 mkdir -p "$output_directory"
 
-echo "Extracting frames from $input_file every $interval seconds."
-
 time_to_seconds() {
     IFS=: read -r hour minute second <<< "$1"
     echo $(( 10#$hour*3600 + 10#$minute*60 + 10#$second ))
@@ -32,9 +30,11 @@ end_seconds=$(time_to_seconds "$end_time")
 duration=$(( end_seconds - start_seconds ))
 interval=$(echo "scale=6; $duration / $number_of_frames" | bc)
 
+echo "Extracting frames from $input_file every $interval seconds."
+
 for ((i=0; i<number_of_frames; i++)); do
     current_offset=$(echo "scale=6; $start_seconds + $i * $interval" | bc)
-    ffmpeg -y -hwaccel videotoolbox -ss "$current_offset" -i "$file_path" -vframes 1 -q:v 2 -f image2 -y -loglevel error "$output_directory/$input_file$(printf "%03d" $i).png"
+    ffmpeg -y -ss "$current_offset" -i "$file_path" -vframes 1 -q:v 2 -f image2 -y -loglevel error "$output_directory/$input_file$(printf "%03d" $i).png"
 done
 
 echo "Extraction complete. Frames saved in $output_directory."
